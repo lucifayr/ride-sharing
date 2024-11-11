47fba3069aa95a1b5d111e08156af977b7a925ad
lucifayr <lucygschwantner@gmail.com>
2024-11-11 19:40:58 +0100

    ci: add extra diagnostics

--- deployment/ride-sharing-api-start.sh
+++ deployment/ride-sharing-api-start.sh
@@ -1,3 +1,6 @@
 systemctl daemon-reload
 systemctl start ride-sharing-api.service
 systemctl restart ride-sharing-api.service
+
+sleep 3
+systemctl status ride-sharing-api.service
--- deployment/ride-sharing-web-app-start.sh
+++ deployment/ride-sharing-web-app-start.sh
@@ -1,3 +1,6 @@
 systemctl daemon-reload
 systemctl start ride-sharing-web-app.service
 systemctl restart ride-sharing-web-app.service
+
+sleep 3
+systemctl status ride-sharing-web-app.service
