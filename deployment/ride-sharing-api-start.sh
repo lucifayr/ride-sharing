c3efd004ab91157a724ea8583a587d23d5f45b35
lucifayr <lucygschwantner@gmail.com>
2024-11-11 19:37:30 +0100

    update example deployment scripts

--- deployment/ride-sharing-api-start.sh
+++ deployment/ride-sharing-api-start.sh
@@ -1,4 +1,3 @@
-cp --force deployment/ride-sharing-api.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start ride-sharing-api.service
 systemctl restart ride-sharing-api.service
--- deployment/ride-sharing-web-app-start.sh
+++ deployment/ride-sharing-web-app-start.sh
@@ -1,4 +1,3 @@
-cp --force deployment/ride-sharing-web-app.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start ride-sharing-web-app.service
 systemctl restart ride-sharing-web-app.service
