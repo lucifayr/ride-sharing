f3256241f33c037b473ef8f45a89b082c05fbbe7
lucifayr <lucygschwantner@gmail.com>
2024-12-03 19:04:43 +0100

    restrict CORS allowed methods

--- api-server/app/rest/cors.go
+++ api-server/app/rest/cors.go
@@ -1,17 +1,15 @@
 package rest
 
 import (
-	"log"
 	"net/http"
 )
 
 func WithCors(mux *http.ServeMux) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header()["Access-Control-Allow-Origin"] = []string{"*"}
-		w.Header()["Access-Control-Allow-Methods"] = []string{"*"}
+		w.Header()["Access-Control-Allow-Methods"] = []string{"GET", "POST", "OPTIONS"}
 		w.Header()["Access-Control-Allow-Headers"] = []string{"*"}
 
-		log.Println("Request ", r)
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(200)
 		} else {
