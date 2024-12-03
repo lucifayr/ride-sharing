33f7d7283fd2cba0768bd85be991b242648ae83d
lucifayr <lucygschwantner@gmail.com>
2024-12-03 19:09:41 +0100

    CORS only allow origin web app

--- api-server/app/rest/cors.go
+++ api-server/app/rest/cors.go
@@ -2,11 +2,13 @@ package rest
 
 import (
 	"net/http"
+	"ride_sharing_api/app/common"
+	"ride_sharing_api/app/utils"
 )
 
 func WithCors(mux *http.ServeMux) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header()["Access-Control-Allow-Origin"] = []string{"*"}
+		w.Header()["Access-Control-Allow-Origin"] = []string{utils.GetEnvRequired(common.ENV_WEB_APP_URL)}
 		w.Header()["Access-Control-Allow-Methods"] = []string{"GET", "POST", "OPTIONS"}
 		w.Header()["Access-Control-Allow-Headers"] = []string{"*"}
 
